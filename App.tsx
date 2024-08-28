9342be7847ed4cf5c12dbdb153086a6bbf508a90
tanadendere <tanadendere@gmail.com>
2024-08-28 10:25:43 +0200

    feat: :sparkles: Added a modal

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
-import { useEffect, useReducer, useCallback } from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { useEffect, useReducer, useCallback, useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 import { getList } from "./api/picsum";
 import { actionCreators, initialState, reducer } from "./reducers/photos";
@@ -7,6 +15,7 @@ import PhotoGrid from "./components/PhotoGrid";
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const { photos, nextPage, loading, error } = state;
 
@@ -32,7 +41,7 @@ export default function App() {
   if (photos.length === 0) {
     if (loading) {
       return (
-        <View style={styles.container}>
+        <View style={styles.centeredView}>
           <ActivityIndicator animating={true} />
         </View>
       );
@@ -40,7 +49,7 @@ export default function App() {
 
     if (error) {
       return (
-        <View style={styles.container}>
+        <View style={styles.centeredView}>
           <Text>Failed to load photos!</Text>
         </View>
       );
@@ -49,7 +58,36 @@ export default function App() {
 
   // fetching the next page of photos everytime onEndReached is called
   return (
-    <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos} />
+    <View style={styles.centeredView}>
+      <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos} />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>This is my 3/4 modal</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+    </View>
   );
 }
 
@@ -59,5 +97,48 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    width: "75%",
+    height: "75%",
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
